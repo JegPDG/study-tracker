9ff281be80f88d75cdbad3de90a3bb7385b3b38c
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-07-04 23:40:39 +0800

    fixing new subjects post

--- frontend/src/pages/NewSubject.jsx
+++ frontend/src/pages/NewSubject.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './newsubject.css'
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import api from '../services/api';
 
 const NewSubject = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const {authToken} = useContext(AuthContext)
+  const navigate = useNavigate();
+
+  const submitNewSubject = async(e) => {
+    e.preventDefault();
+    try {
+      await api.post('subject/', 
+        { "title": title,
+          "content": content },
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      )
+      console.log('Subject created')
+      navigate('/subject')
+    } catch (error) {
+      console.log(error)
+      console.log('error in creating subject')
+    }
+
+  }
+
   return (
    <>
    <div className="main-new-subject">
       <div className="new-subject-page">
+        <p className='sub-heading'>Create new subject</p>
+        <div className="form-content">
+          <form onSubmit={submitNewSubject}>
+            <p className='sub-heading'>Subject title</p>
+            <input className="input-title-card"
+                  placeholder='New Subject title'
+                  type='text'
+                  onChange={(e) => setTitle(e.target.value)}
+                  >
+            </input>
+
+            <p className='sub-heading'>Description</p>
+            <input className="input-description-card"
+                  placeholder='Description'
+                  type='text'
+                  onChange={(e) => setContent(e.target.value)}
+
+              >
+            </input>
+
+            <div className="but-cont">
+              <button type='submit'>Save Subject</button>
+            </div>
+          </form>
+
+
+
+
+
+        </div>
+
 
 
       </div>

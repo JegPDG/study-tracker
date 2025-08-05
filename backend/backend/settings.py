b27d8e9afe122c156fcb95c171cd6a19bafedda8
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-08-05 21:34:43 +0800

    Initial deployment commits

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+24%0e$ufg6q!-^!f=@nin-(xmkx04@f)#we!4+@pokqea&$^f'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -82,10 +85,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -124,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
-from .views import SubjectsViewSet, SubjectDashboardViewSet
+from .views import SubjectsViewSet, SubjectDashboardViewSet, NoteDashboardView
 
 urlpatterns = [
   path('', views.index),
-   path('dashboard/subjects/', SubjectDashboardViewSet.as_view(), name='subject-dashboard'),
+  path('dashboard/subjects/', SubjectDashboardViewSet.as_view(), name='subject-dashboard'),
+  path('dashboard/notes/',  NoteDashboardView.as_view(), name='note-dashboard'),
 ]
 
 router = DefaultRouter()

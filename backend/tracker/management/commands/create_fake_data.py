84a1fac2f9dbff05cc96597e04e9f3e6329f1774
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-08-14 13:21:04 +0800

    Added faker and admin for model admin for users

--- backend/tracker/management/commands/create_fake_data.py
+++ backend/tracker/management/commands/create_fake_data.py
@@ -6,11 +6,11 @@ from users.models import User
 fake = Faker()
 
 class Command(BaseCommand):
-  help ='Genrate Subject and Notes. Command should be python manage.py create_fake_data --user 10 --subject 10 --notes 10'
+  help ='Genrate Subject and Notes. Command should be python manage.py create_fake_data --users 10 --subjects 10 --notes 10'
 
   def add_arguments(self, parser):
         parser.add_argument(
-            '--user',
+            '--users',
             type=int,
             default=5,
             help='Number of users to generate is (default is 10)',
@@ -29,17 +29,44 @@ class Command(BaseCommand):
         )
   
   def handle(self, *args, **options):
-      user_count = options['user']
+      user_count = options['users']
       subject_count = options['subjects']
       notes_count = options['notes']
 
       users = []
 
-      # for _ in range(users):
+      for _ in range(user_count):
+          user = User.objects.create_user(
+              username=fake.unique.user_name(),
+              email=fake.unique.email(),
+              password='password'
+          )      
+          users.append(user)
+
+      self.stdout.write(self.style.NOTICE('Successfully added fake users!'))
+      
+      for i in range(user_count):
+        self.stdout.write(self.style.NOTICE(f'users: {users[i].username}'))
+
+      for user in users:
+          self.stdout.write(self.style.NOTICE(f'user:{user.username}')) 
+          for _ in range(subject_count):
+            subject = Subject.objects.create(
+                name=fake.sentence(nb_words=4),
+                description=fake.sentence(nb_words=30),
+                user=user
+            )
           
-           
+            self.stdout.write(self.style.NOTICE(f'subject:{subject.name}'))
 
 
+            for _ in range(notes_count):
+                note = Notes.objects.create(
+                    subject=subject,
+                    title=fake.sentence(nb_words=5),
+                    content=fake.paragraph(nb_sentences=10)
+                )
+                self.stdout.write(self.style.NOTICE(f'note:{note.title}'))
 
-
-      return super().handle(*args, **options)
+      self.stdout.write(self.style.SUCCESS(f'Successfully created {user_count} users with {subject_count} subjects and {notes_count} notes'))
+      
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User
+from tracker.models import Subject
+
+class SubjectTabularInlines(admin.TabularInline):
+  model = Subject
+  raw_id_fields = ['user']
+  autocomplete_fields = ['user']
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+  list_display = ['username', 'email']
+  search_fields= ['username']
+
+  inlines=[SubjectTabularInlines]
+
+  def has_delete_permission(self, request, obj = ...):
+    return True
+
+admin.site.register(User, UserAdmin)
